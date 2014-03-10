f994431998ae6982f4275f3f66b6aba633385315
yawol <yuronir@gmail.com>
2014-03-10 17:23:39 +0900

    only one line remain

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -4,9 +4,6 @@ namespace helloworld {
 	class MainClass {
 		public static void Main (string[] args){
 			Console.WriteLine("ㅇㅏㄴㄴㅕㅇㅎㅏㅅㅔㅇㅛ!");
-			Console.WriteLine ("Why not Korean!!!!!");
-			Console.WriteLine ("test commit with add");
-			Console.WriteLine ("i love you");
 		}
 	}
 }
